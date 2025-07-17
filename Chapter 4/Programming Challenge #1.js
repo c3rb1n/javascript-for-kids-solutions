852d8f9fbbbc3e5223525c64862333b08c2188f0
c3rb1n <ilya.prudnikov.97@gmail.com>
2025-07-17 07:50:18 +0300

    added solution for "Programming Challenge #1" task from chapter 4

---
+++ Chapter 4/Programming Challenge #1.js
@@ -0,0 +1,17 @@
+const scores = {
+    john: 0,
+    pete: 14,
+    jack: 6
+};
+
+// Increase John's scores by 30
+scores.john += 10;
+
+// Increase Pete's scores by 10
+scores.pete += 20;
+
+// Increase Jack's scores by 20
+scores.jack += 30;
+
+// Show scores
+scores; // {john: 10, pete: 34, jack: 36}
