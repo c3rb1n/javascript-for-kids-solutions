22d9c5fd275fd18e2322661b4b60991c3f25aa34
c3rb1n <ilya.prudnikov.97@gmail.com>
2026-02-13 21:46:11 +0300

    added files refactoring for "Programming Challenge #1" exercise from Chapter 6

---
+++ Chapter 6/Programming Challenge #1/main.js
@@ -0,0 +1,7 @@
+const animals = ['Кот', 'Рыба', 'Лемур', 'Комодский варан'];
+
+for (let index = 0; index < animals.length; index++) {
+    animals[index] = `${animals[index]} - прекрасное животное`;
+}
+
+console.log(animals);
