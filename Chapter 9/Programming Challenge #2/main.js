db5232a8d0f7222ad32e02790bd23fee0576956d
c3rb1n <ilya.prudnikov.97@gmail.com>
2026-02-22 20:45:52 +0300

    added files refactoring for "Programming Challenge #2" exercise from Chapter 9

---
+++ Chapter 9/Programming Challenge #2/main.js
@@ -0,0 +1,13 @@
+$('h1').fadeOut(500).fadeIn(500);
+$('h1').fadeOut(500).fadeIn(500);
+$('h1').fadeOut(500).fadeIn(500);
+$('h1').fadeOut(500).fadeIn(500);
+$('h1').fadeOut(500).fadeIn(500);
+
+for (let index = 0; index < 5; index++) {
+    $('h1').fadeOut(500).fadeIn(500);
+}
+
+for (let index = 1; index <= 5; index++) {
+    $('h1').fadeOut(500 * index).fadeIn(500 * index);
+}
