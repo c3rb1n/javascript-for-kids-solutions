04b3ba97381e67b91c4a73e7a22b992eead84b03
c3rb1n <ilya.prudnikov.97@gmail.com>
2026-03-23 22:51:11 +0300

    removed comments in "Programming Challenge #1.js" file from Chapter 4

--- Chapter 4/Programming Challenge #1.js
+++ Chapter 4/Programming Challenge #1.js
@@ -4,14 +4,8 @@ const scores = {
     jack: 6
 };
 
-// Increase John's scores by 30
 scores.john += 10;
-
-// Increase Pete's scores by 10
 scores.pete += 20;
-
-// Increase Jack's scores by 20
 scores.jack += 30;
 
-// Show scores
-scores; // {john: 10, pete: 34, jack: 36}
+scores;
